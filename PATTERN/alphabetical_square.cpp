36250d024aab4af03432bf034a385cf840b4d799
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-05 16:55:01 +0530

    UPDATE PATTERN

--- PATTERN/alphabetical_square.cpp
+++ PATTERN/alphabetical_square.cpp
@@ -16,7 +16,7 @@ int main(){
     return 0;
 }
 */
-
+/*
 #include<iostream>
 using namespace std;
 int main(){
@@ -26,6 +26,22 @@ int main(){
     for (int i=0; i<n; i++){
         for (int j=0; j<n; j++){
             cout<<(char)(j+65)<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"ENTER A NUMBER: ";
+    cin>>n;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cout<<(char)(j+97)<<" ";
         }
         cout<<endl;
     }
