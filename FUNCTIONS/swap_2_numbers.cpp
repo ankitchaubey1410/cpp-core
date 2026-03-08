2e74a4029750a8e5e20ff4755dc79518f9d3bf1e
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-08 15:24:17 +0530

    update swapping of two numbers

--- FUNCTIONS/swap_2_numbers.cpp
+++ FUNCTIONS/swap_2_numbers.cpp
@@ -1,2 +1,67 @@
+/*
 #include<iostream>
 using namespace std;
+void swap(int x, int y){
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    cout<<"new values (x : y) = "<<x<<" : "<<y<<endl;
+    return;
+}
+int main(){
+    int x, y;
+    cout<<"enter x : ";
+    cin>>x;
+    cout<<"enter y : ";
+    cin>>y;
+    cout<<"current values (x : y) = "<<x<<" : "<<y<<endl;
+    swap(x, y);
+    return 0;
+}
+*/
+
+// swapping is happenning only inside function not in main() because from main() function is taking copied value not actual value
+// Pass by value
+/*
+#include<iostream>
+using namespace std;
+void swap(int x, int y){
+    int temp = x;
+    x = y;
+    y = temp;
+    return;
+}
+int main(){
+    int x, y;
+    cout<<"enter x : ";
+    cin>>x;
+    cout<<"enter y : ";
+    cin>>y;
+    cout<<"current values (x : y) = "<<x<<" : "<<y<<endl;
+    swap(x, y);
+    cout<<"new values (x : y) = "<<x<<" : "<<y<<endl;
+    return 0;
+}
+*/
+
+//now function is taking actual values from main 
+//Pass by reference
+#include<iostream>
+using namespace std;
+void swap(int& x, int& y){
+    int temp = x;
+    x = y;
+    y = temp;
+    return;
+}
+int main(){
+    int x, y;
+    cout<<"enter x : ";
+    cin>>x;
+    cout<<"enter y : ";
+    cin>>y;
+    cout<<"current values (x : y) = "<<x<<" : "<<y<<endl;
+    swap(x, y);
+    cout<<"new values (x : y) = "<<x<<" : "<<y<<endl;
+    return 0;
+}
