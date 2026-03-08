5583bfb7c79c29922147da96bb7c90c7713f039a
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-08 12:38:52 +0530

    update pascal triangle left align

--- FUNCTONS/pascal_triangle_left_align.cpp
+++ FUNCTONS/pascal_triangle_left_align.cpp
@@ -51,7 +51,7 @@ int main(){
     int n;
     cout<<"enter a number : ";
     cin>>n;
-    for (int i =  0; i < n; i++){
+    for (int i =  0; i <= n; i++){
         for (int j = 0; j <= i; j++){
             cout<<ncr(i, j)<<" ";
         }
