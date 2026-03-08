cc47fe1b31b8c00249c87723c76ad872e3d1ce8d
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-08 14:41:54 +0530

    add swapping of two numbers

---
+++ BASICS/swapping_of_2.cpp
@@ -0,0 +1,30 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 2;
+    int y = 5;
+    cout<<"current values : "<<x<<"  "<<y<<endl;
+    int z = x;
+    x = y;
+    y = z;
+    cout<<"new values : "<<x<<"  "<<y;
+    
+    return 0;
+}
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x, y;
+    cout<<"enter first value : ";
+    cin>>x;
+    cout<<"enter second value : ";
+    cin>>y;
+    cout<<"current values (x : y) = "<<x<<" : "<<y<<endl;
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    cout<<"new values (x : y) = "<<x<<" : "<<y<<endl;
+}
