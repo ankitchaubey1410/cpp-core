cbbddf73485b232fce376416ff9a134668eb7ef4
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-09 17:13:18 +0530

    add pointers address

---
+++ POINTERS/address.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 3;
+    int y = 3;
+    cout<<"address of x : "<<&x<<endl;
+    cout<<"address of y : "<<&y<<endl;
+    return 0;
+}
