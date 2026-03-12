30ce6456af9147c0c29a5ff42dcad109ab538ce5
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-12 21:37:03 +0530

    Create india_usa.cpp

---
+++ india_usa.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void usa(){
+    cout<<"YOU ARE IN USA!!"<<endl;
+    return;//Function get finished as soon as return statement found
+}
+void india(){
+    cout<<"YOU ARE IN INDIA!!"<<endl;
+    return;
+}
+int main(){
+    cout<<"YOU ARE IN MAIN";
+    usa();
+    india();
+}
