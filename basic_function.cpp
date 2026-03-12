59d687e8f234c2fe83376fee72987b0c1c4b11a0
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-12 21:31:48 +0530

    Create basic_function.cpp

---
+++ basic_function.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void greet(){
+    cout<<"GOOD MORNING";
+    cout<<"How are you??";
+}
+int main(){
+    greet();
+    cout<<"HII BABY😘";
+    greet();
+}
