867eba81a77c1301c6ccc2f8ee0918dcb938c06b
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-12 23:14:44 +0530

    UPDATE alphanum

--- PATTERN/alphanum_triangle.cpp
+++ PATTERN/alphanum_triangle.cpp
@@ -1,26 +1,3 @@
-/*
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    cout << "ENTER A NUMBER: ";
-    cin >> n;
-    int N = 64;
-    for (int i = 1; i <= n; i=i+2){
-        for (int j = 1; j <= i; j++){
-            cout << (char)(j + N) << " ";
-        }
-        cout << endl;
-    }
-    for (int i = 1; i <= n; i=i+2){
-        for (int j = 1; j <= i; j++){
-            cout << j << " ";
-        }
-        cout<<endl;
-    }
-    return 0;
-}
-*/
 #include <iostream>
 using namespace std;
 
