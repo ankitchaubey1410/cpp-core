ff05a5f14690b23d8b3efd6dde3c304a267d858c
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-16 00:52:13 +0530

    update OOPS dynamic allocation

--- OOPS/dynamic.cpp
+++ OOPS/dynamic.cpp
@@ -15,13 +15,21 @@ public:
 };
 
 int main(){
-    // Cricketers c1("Maki" , 99000 , 55.6);
-    // Cricketers c2("Mai" , 88000 , 48.7);
+    Cricketers c1("Maki" , 99000 , 55.6);
+    Cricketers* c2 = new Cricketers("Mai" , 88000 , 48.7);
+    cout<<c1.name<<" "<<c1.avg<<" "<<c1.runs<<endl;
+    // cout<<(*c2).name<<" "<<(*c2).avg<<" "<<(*c2).runs<<endl;
+    cout<<c2->name<<" "<<c2->avg<<" "<<c2->runs<<endl;
+    c2->name = "Yuta";
+    cout<<c2->name<<" "<<c2->avg<<" "<<c2->runs<<endl;
 
     // Dynamic Allocation
-    int* ptr = new int(9999); // In runtime memory allocate
-    cout<<*ptr<<endl;
-    cout<<&(*ptr)<<endl;
+    // int* ptr = new int(9999); // In runtime memory allocate
+    // cout<<*ptr<<endl;
+    // cout<<&(*ptr)<<endl;
     
+     
+
+
     return 0;
 }
