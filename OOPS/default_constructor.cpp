afcc83d7a4dfee5faab870430f3c5a9ae93f8159
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-18 14:26:52 +0530

    update OOPS default constructor

--- OOPS/default_constructor.cpp
+++ OOPS/default_constructor.cpp
@@ -24,16 +24,20 @@ int main(){
 
     Student s1("reze" , 69 );
     s1.cgpa = 8.8;
+    // s1.rno = 56; // without default constructor it will run
 
-    Student s2;
+
+    Student s2; // without default constructor it will not run and error in declaration 
     s2.name = "rika";
     s2.rno = 88;
     s2.cgpa = 7.9;
-
+    
+    Student s3;
 
     
     cout<<s1.name<<" "<<s1.cgpa<<" "<<s1.rno<<endl;
     cout<<s2.name<<" "<<s2.cgpa<<" "<<s2.rno<<endl;
+    cout<<s3.name<<" "<<s3.cgpa<<" "<<s3.rno<<endl; // 2.33422e-038 6422204 it will print garbage value and different ones everytime code is executed
     
     return 0;
 }
