71f96b78872455dc6cf08dee23d3239e3a0e7ec1
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-22 01:24:53 +0530

    update second largest in an array

--- ARRAYS/second_largest.cpp
+++ ARRAYS/second_largest.cpp
@@ -1,12 +1,14 @@
 // Second Largest element in an array
 
 #include<iostream>
+#include <climits> 
 using namespace std;
 int main(){
     int arr[] = {88,55,64,94,121,1,24,21,34};
     int n = sizeof(arr)/sizeof(arr[0]);
     int mx = arr[0];
-    int smx = -1;
+    // int smx = -1; // if array doesn't contain any negative number
+    int smx = INT_MIN; // it is the smallest number that a interger can contain
     for (int i = 1; i < n; i++){
         if (arr[i] > mx){
             mx = arr[i];
