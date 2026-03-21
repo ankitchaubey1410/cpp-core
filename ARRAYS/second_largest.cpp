cbf2b76422f9e51c9b1f779efa5fc46237f95f74
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-22 01:45:36 +0530

    update second largest in an array

--- ARRAYS/second_largest.cpp
+++ ARRAYS/second_largest.cpp
@@ -1,5 +1,6 @@
 // Second Largest element in an array
 
+/*
 #include<iostream>
 #include <climits> 
 using namespace std;
@@ -20,6 +21,30 @@ int main(){
         }
     }
     cout<<"first maximum in an array : "<<mx<<endl;
+    cout<<"second maximum in an array : "<<smx<<endl;
+    return 0;
+}
+*/
+
+// Second Largest element in an array
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {88,155,64,94,121,120,24,21,34};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int mx = arr[0];
+    int smx = -1;
+    for (int i = 1; i < n; i++){
+        if (arr[i] > mx){
+            smx = mx;
+            mx = arr[i];
+        }
+        if (arr[i] < mx && arr[i] > smx){
+            smx = arr[i];
+        }
+    }
+    cout<<"maximum in an array : "<<mx<<endl;
     cout<<"second maximum in an array : "<<smx<<endl;
     return 0;
 }
