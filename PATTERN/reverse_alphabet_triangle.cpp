9154a0b09bd700710fe9ab08aec7bbccd67b7631
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-21 23:46:42 +0530

    update reverse triangle of alphabet

--- PATTERN/reverse_alphabet_triangle.cpp
+++ PATTERN/reverse_alphabet_triangle.cpp
@@ -1,31 +1,36 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter a number : ";
-//     cin>>n;
-//     for (int i = 1; i <= n; i++){
-//         for (int j = 0; j <= n - i; j++){
-//             cout<<(char)(65+j);
-//         }
-//         cout<<endl;
-//     }
-// }
+// Reversed Alphabet Triangle 
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number : ";
+    cin>>n;
+    for (int i = 1; i <= n; i++){
+        for (int j = 0; j <= n - i; j++){
+            cout<<(char)(65+j);
+        }
+        cout<<endl;
+    }
+}
+*/
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter a number : ";
-//     cin>>n;
-//     for (int i = 1; i <= n; i++){
-//         for (int j = 0; j <= n - i; j++){
-//             cout<<(char)(64+i);
-//         }
-//         cout<<endl;
-//     }
-// }
-
+/*
+#include<iostream>
+using namespace std;
+int main(){
+        int n;
+    cout<<"enter a number : ";
+    cin>>n;
+    for (int i = 1; i <= n; i++){
+            for (int j = 0; j <= n - i; j++){
+                    cout<<(char)(64+i);
+                }
+                cout<<endl;
+            }
+        }
+*/
+        
 #include<iostream>
 using namespace std;
 int main(){
