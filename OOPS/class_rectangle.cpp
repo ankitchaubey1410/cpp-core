f51c7e7d6fd4fb030aa08296534c41580162710a
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-22 14:26:44 +0530

    add class rectangle area and perimeter

--- OOPS/class_rectangle.cpp
+++ OOPS/class_rectangle.cpp
@@ -9,18 +9,19 @@ public:
 
     float area(){
         return length * breadth;
+    }
     float perimeter(){
         return 2 * (length + breadth);
     }
 };
 
-int main() {
+int main(){
     Rectangle r1;
     r1.length = 12;
     r1.breadth = 10;
 
-    cout<<"Area = "<<r1.area();
+    cout<<"Area = "<<r1.area()<<endl;
 
-    cout<<"Perimeter = "<<r1.perimeter();
+    cout<<"Perimeter = "<<r1.perimeter()<<endl;
     return 0;
 }
