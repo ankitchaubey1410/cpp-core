de27a313620b8992d8a074fc9c2d511d5f6f3f2c
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-24 18:26:19 +0530

    add sorting in vectors

---
+++ ARRAYS/reversed_array.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[] = {1, 2, 4, 5, 9, 7, 8, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout<<"Original Array : "<<endl;
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Reversed Array : "<<endl;
+    int reversed_arr[n];
+    for(int i = 0; i < n; i++){
+        reversed_arr[i] = arr[n - 1 - i];
+    }
+    for(int i = 0; i < n; i++){
+        cout<<reversed_arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
