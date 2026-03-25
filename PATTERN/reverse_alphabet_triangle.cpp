3c74eac99be4f3e529f1e0059ead3d81200f9f57
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-25 23:39:05 +0530

    update reverse triangle of alphabet

--- PATTERN/reverse_alphabet_triangle.cpp
+++ PATTERN/reverse_alphabet_triangle.cpp
@@ -1,3 +1,17 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter a number : ";
+//     cin>>n;
+//     for (int i = 1; i <= n; i++){
+//         for (int j = 0; j <= n - i; j++){
+//             cout<<(char)(65+j);
+//         }
+//         cout<<endl;
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -6,7 +20,7 @@ int main(){
     cin>>n;
     for (int i = 1; i <= n; i++){
         for (int j = 0; j <= n - i; j++){
-            cout<<(char)(65+j);
+            cout<<(char)(64+i);
         }
         cout<<endl;
     }
