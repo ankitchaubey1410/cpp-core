9e644e94468e4c2b303e600d06ae283e526b65fb
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-26 17:20:18 +0530

    update size of an arrya

--- ARRAYS/size_of_arr.cpp
+++ ARRAYS/size_of_arr.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9,};
-    cout<<"size of an array : "<<(sizeof(arr)/sizeof(int));
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    // cout<<"size of an array : "<<(sizeof(arr)/sizeof(int));
+    cout<<"size of an array : "<<(sizeof(arr)/sizeof(arr[0]));
     return 0;
 }
