e29e17eec0d2ae12a2aad2cb41c60dd5f6b98f31
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-26 17:38:20 +0530

    add variable size array

---
+++ ARRAYS/Variable_size_arr.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter size of an array : ";
+    cin>>n;
+    int arr[n];
+    
+    for (int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    for (int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
