d534cf93bae1b6c0fc22a58c08aacfd267d66c22
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-29 13:33:07 +0530

    add identity matrix

---
+++ ARRAYS/identity_matrix.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a size of square matrix : ";
+    cin>>n;
+    int arr[n][n];
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (i == j){
+                arr[i][j] = 1;
+            }
+            else{
+                arr[i][j] = 0;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
