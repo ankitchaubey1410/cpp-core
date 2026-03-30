db45e93580e80d8cd8ccca894bfd6f45523ac14d
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-03-30 14:25:14 +0530

    add pair functions in stl

---
+++ STL/pair.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void explainpair(){
+    pair<int, int> pr1 = {1, 2};
+    cout<<pr1.first<<" "<<pr1.second<<endl;
+    pair<int, string> pr2 = {99, "Sweetylemon"};
+    cout<<pr2.first<<" "<<pr2.second<<endl;
+}
+int main(){
+    explainpair();
+    return 0;
+}
