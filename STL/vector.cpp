5b45376bd3e87d8878c64825b799966c908153d6
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-04-02 19:43:49 +0530

    update vectors

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -15,11 +15,21 @@ void explainvector(){
     for (int i = 0; i < vec.size(); i++){
         cout<<vec[i]<<" ";
     }
+    cout<<endl;
 
     vector<int> vec2(4, 0); // vector of size 4 with all elements as 0
     for (int i = 0; i < vec2.size(); i++){
         cout<<vec2[i]<<" ";
     }
+    cout<<endl;
+    
+    // Iterators - It is an object which points to an element of a container and has the ability to iterate through the elements of the container.
+    vector<int> :: iterator beginITR = vec.begin();
+    vector<int> :: iterator endITR = vec.end();
+    for (vector<int> :: iterator i = beginITR; i < endITR; i++){
+        cout<<*i<<" ";
+    }
+    cout<<endl;
 }
 int main(){
     explainvector();
