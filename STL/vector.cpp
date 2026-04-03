4c57902d80f7336588a7be7a489e330568b92112
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-04-03 19:01:29 +0530

    add vectors

---
+++ STL/vector.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void explainvector(){
+    vector<int> vec;
+    vec.push_back(-1);
+    vec.push_back(99);
+    vec.push_back(-14);
+    vec.push_back(0);
+    // Instead of using push_back you can use emplace_back
+    vec.emplace_back(6);
+    cout<<vec.size()<<endl;
+
+}
+int main(){
+    explainvector();
+    return 0;
+}
