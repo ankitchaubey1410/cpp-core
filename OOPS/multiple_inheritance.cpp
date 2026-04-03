09e51f8e56fa9777c84478730f621dac7b474ecb
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-04-03 23:35:35 +0530

    update multiple inheritance

--- OOPS/multiple_inheritance.cpp
+++ OOPS/multiple_inheritance.cpp
@@ -1,2 +1,35 @@
 #include<iostream>
 using namespace std;
+class Cricketer{
+public: 
+    int runs;
+    int wickets;
+    int average;
+};
+class Engineer{
+public: 
+    int experience;
+    string domain;
+};
+class Psycho : public Engineer, Cricketer{ // multiple inheritance
+public:
+    string name;
+}
+int main(){
+    Psycho p;
+    p.name = "John Doe";
+    p.experience = 5;
+    p.domain = "Software";
+    p.runs = 1000;
+    p.wickets = 50;
+    p.average = 40;
+    
+    cout << "Name: " << p.name << endl;
+    cout << "Experience: " << p.experience << " years" << endl;
+    cout << "Domain: " << p.domain << endl;
+    cout << "Runs: " << p.runs << endl;
+    cout << "Wickets: " << p.wickets << endl;
+    cout << "Average: " << p.average << endl;
+    
+    return 0;
+}
