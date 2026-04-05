7e3112f7ee51a23ce0844d919f21409cee50d592
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-04-05 23:14:56 +0530

    add single inheritance of vehicle class

---
+++ OOPS/vehicle_class.cpp
@@ -0,0 +1,24 @@
+// Parent class with multiple Child classes
+#include<iostream>
+using namespace std;
+class Vehicle{
+public:
+    int topSpeed;
+    float mileage;
+    string fuel;
+};
+class Car : public Vehicle{
+public:
+    
+};
+class Bike : public Vehicle{
+public:
+    
+};
+class Truck : public Vehicle{
+public:
+    
+};
+int main(){
+    return 0;
+}
