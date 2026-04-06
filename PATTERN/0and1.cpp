c51e1273582cc0f9a1a1c57db67667c704a8fc01
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-04-06 11:48:17 +0530

    add 0 and 1 triangle

--- PATTERN/0and1.cpp
+++ PATTERN/0and1.cpp
@@ -1,8 +1,32 @@
+// Square pattern of 0 and 1
+/*
 #include<iostream>
 using namespace std;
 int main(){
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 5; j++){
+            if((i + j) % 2 == 0){
+                cout << "1 ";
+            } else {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
+*/
+
+// Triangle pattern of 0 and 1
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number : ";
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
             if((i + j) % 2 == 0){
                 cout << "1 ";
             } else {
