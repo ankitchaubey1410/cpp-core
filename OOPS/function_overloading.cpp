31b005cdcdc507de1098920482a6571b2d394531
ANKIT CHAUBEY <ankitchaubey1421@gmail.com>
2026-04-08 01:46:32 +0530

    add function overloading

---
+++ OOPS/function_overloading.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Loading{
+public:
+    void sum(string a,string b){
+        cout<<a+b<<endl;
+    }
+    void sum(int a,int b){
+        cout<<a+b<<endl;
+    }
+    // int sum(int a,int b){ // error
+    //     return a+b;
+    // }
+    void sum(int a,int b,int c){
+        cout<<a+b+c<<endl;
+    }
+};
+int main(){
+    Loading a;
+    a.sum(69,88);
+    a.sum(19,89,67);
+    a.sum("Anushka"," Bitch");
+}
