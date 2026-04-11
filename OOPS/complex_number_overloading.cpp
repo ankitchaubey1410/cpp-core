72552bda5357af637f356dc95e80d035c48e21e6
ankitchaubey1410 <ankitchaubey1421@gmail.com>
2026-04-11 15:51:05 +0530

    complex number overloading

--- OOPS/complex_number_overloading.cpp
+++ OOPS/complex_number_overloading.cpp
@@ -1,1 +1,30 @@
-#
+#include<iostream>
+using namespace std;
+class Test{
+public:
+    int i;
+    int r;
+
+    Test(int it, int rl){
+        i = it;
+        r = rl;
+    }
+
+    void display(){
+        cout<<r<<" + "<<i<<"i"<<endl;
+    }
+
+    Test operator+(Test x){
+        Test c(0,0);
+        c.r = r + x.r;
+        c.i = i + x.i;
+        return c;
+    }
+};
+int main(){
+    Test t1(3, 4);
+    Test t2(11, 25);
+    Test t3 = t1 + t2;
+    t3.display();
+    return 0;
+}
