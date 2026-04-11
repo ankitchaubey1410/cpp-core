7a16dbb3f943422afc8ad1ac1eedc57189ad5fd8
ankitchaubey1410 <ankitchaubey1421@gmail.com>
2026-04-11 18:52:23 +0530

    add friend function

---
+++ OOPS/friend_function.cpp
@@ -0,0 +1,23 @@
+// A friend class is a special class that has access to the private and protected members of another class.
+// #include <iostream>
+// using namespace std;
+// class X{
+// private:
+//     int a = 5;
+
+//     friend class Y; // Y is a friend of Class X, so Y has all access to the private and protected members of Class X
+// };
+// class Y{
+// public:
+//     void display(X obj){
+//         cout<<"Value of a is : "<<obj.a<<endl;
+//     }
+
+// };
+// int main(){
+//     X obj1;
+//     Y obj2;
+//     obj2.display(obj1);
+//     return 0;
+// }
+
