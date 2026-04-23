15c72354da41b642580029661b38b1b64cfda781
ankitchaubey1410 <ankitchaubey1421@gmail.com>
2026-04-24 00:03:04 +0530

    increment operator overloading

--- OOPS/increment_operator_overloading.cpp
+++ OOPS/increment_operator_overloading.cpp
@@ -13,19 +13,22 @@ public:
     }
 
     Test operator++(int){
-        aa = aa + 1;
+        aa = aa + 1; return *this;
     }
     Test operator++(){
-        aa = aa + 1;
+        aa = aa + 1; return *this;
     }
 };
 int main(){
     Test t1(5);
+    cout<<"Before increment : "<<endl;
     t1.display();
     ++t1; // pre increment
+    cout<<"After pre increment : "<<endl;
     t1.display();
     t1++; // post increment
     // cout<<t1.aa;
+    cout<<"After post increment : "<<endl;
     t1.display();
 
 }
